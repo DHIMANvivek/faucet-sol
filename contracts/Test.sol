153629fab6430055ad88f3c5736fd15821e8bc2e
DHIMANvivek <googlydhiman.4236@gmail.com>
2022-02-13 12:42:45 +0530

    practice sol

---
+++ contracts/Test.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Test{
+   function test(uint testNum) external pure returns(uint){
+
+
+
+
+return testNum;
+
+
+   }
+}
